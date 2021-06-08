c2182eb6bf35b8d89fee2d0ef8aba8d0e832108b
Daniel Petti <djpetti@gmail.com>
2021-06-08 19:31:19 -0400

    WIP on upload page.

---
+++ mallard/edge/frontend/src/uploader.ts
@@ -0,0 +1,16 @@
+import { LitElement } from "lit-element";
+
+
+/**
+ * An element that allows the user to select and upload files.
+ */
+export class Uploader extends LitElement {
+  static tagName: string = "uploader";
+
+  /**
+   * @inheritDoc
+   */
+  protected render() {
+
+  }
+}

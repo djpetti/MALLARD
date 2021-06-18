7eb64cb00ea0aefd1ffd6d9e115524720d33b347
Daniel Petti <djpetti@gmail.com>
2021-06-18 08:28:52 -0400

    WIP on image upload.

--- mallard/edge/frontend/src/file-uploader.ts
+++ mallard/edge/frontend/src/file-uploader.ts
@@ -1,5 +1,6 @@
 import { css, html, LitElement, property } from "lit-element";
 import "@material/mwc-icon"
+import "@material/mwc-fab"
 
 /**
  * An element that allows the user to select and upload files.
@@ -8,9 +9,23 @@ export class FileUploader extends LitElement {
   /** Tag name for this element. */
   static tagName: string = "file-uploader";
   static styles = css`
+    /** Content on the bottom layer is scrollable and occluded by
+     content on top. */
+    .bottom_layer {
+      position: relative;
+      z-index: 5;
+      background-color: var(--theme-light-gray);
+      overflow: auto;
+    }
+
+    /** Content on the top layer doesn't scroll. */
+    .top_layer {
+      position: fixed;
+      z-index: 10;
+    }
+
     .drop_zone {
-      min-width: 500px;
-      min-height: 100px;
+      height: 100px;
       border-radius: 25px;
       padding: 20px 0;
       border-width: 5px;
@@ -22,9 +37,27 @@ export class FileUploader extends LitElement {
       align-items: center;
     }
 
+    /** Show shadow only along bottom of the drop zone. */
+    #drop_zone_container {
+      min-width: 500px;
+      overflow: hidden;
+      padding-bottom: 10px;
+      margin-left: -24px;
+      margin-right: -24px;
+    }
+
+    /** Place the drop zone on it's own plane above the other content. */
+    #drop_zone_card {
+      padding: 5px 24px 40px 24px;
+      background: white;
+    }
+
     .file_list {
       min-width: 500px;
       min-height: 100px;
+      padding-top: 130px;
+      margin-left: -24px;
+      margin-right: -24px;
     }
 
     #upload_icon {
@@ -52,6 +85,13 @@ export class FileUploader extends LitElement {
     .active_drag {
       border-color: var(--theme-primary);
       color: var(--theme-primary);
+    }
+
+    #browse {
+      position: relative;
+      z-index: 15;
+      top: 167px;
+      right: -90%;
     }
   `;
 
@@ -101,22 +141,28 @@ export class FileUploader extends LitElement {
 
     return html`
       <link rel="stylesheet" href="./static/mallard-edge.css">
-      <div
-        id="upload_drop_zone"
-        class="drop_zone ${dropZoneClass}"
-        @drop="${this.handleDrop}"
-        @dragenter="${this.handleDragEnter}"
-        @dragleave="${this.handleDragLeave}"
-      >
-        <mwc-icon id="upload_icon" class="${dropZoneClass}"
-          >upload_file</mwc-icon
-        >
-        <div class="break"></div>
-        <p id="upload_help" class="${dropZoneClass}">
-          Drag files here to upload.
-        </p>
+      <div id="drop_zone_container" class="top_layer">
+          <div id="drop_zone_card" class="mdc-elevation--z2">
+              <div
+                id="upload_drop_zone"
+                class="drop_zone ${dropZoneClass}"
+                @drop="${this.handleDrop}"
+                @dragenter="${this.handleDragEnter}"
+                @dragleave="${this.handleDragLeave}"
+              >
+                <mwc-icon id="upload_icon" class="${dropZoneClass}"
+                  >upload_file</mwc-icon
+                >
+                <div class="break"></div>
+                <p id="upload_help" class="${dropZoneClass}">
+                  Drag files here to upload.
+                </p>
+              </div>
+          </div>
       </div>
-      <div class="file_list mdc-elevation--z2"></div>
+      <mwc-fab icon="add" id="browse"></mwc-fab>
+      <div class="file_list bottom_layer">
+      </div>
     `;
   }
 }
